c43b7677e41901153dbd6cbed9c5193fb7d0de35
Sebastian Soto <ssoto@redhat.com>
2019-10-25 14:49:04 -0400

    [wsu] Initial e2e framework and test
    This commit includes the e2e framework to spin up a Windows instance,
    run the WSU, and run tests against the instance. It also includes
    a test to ensure that all the files we expect to see on the Windows
    host were copied over correctly

--- hack/run-wsu-ci-e2e-test.sh
+++ hack/run-wsu-ci-e2e-test.sh
@@ -3,6 +3,23 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# TODO: Add steps to execute code that will kick off the e2e tests in CI
+WMCO_ROOT=$(dirname "${BASH_SOURCE}")/..
+TEST_DIR=$WMCO_ROOT/tools/ansible/tasks/wsu/test/e2e
+
+#TODO: Currently exiting early as the CI operator job which runs this does not have pip installed,
+#      which is a requirement to run this script
+exit 0
+
+# Required packages to run the test suite
+sudo yum install -y python libselinux-python python-pip
+pip install --user ansible pywinrm
+
+# The WSU playbook requires the cluster address, we parse that here using oc
+CLUSTER_ADDR=$(oc cluster-info | head -n1 | sed 's/.*\/\/api.//g'| sed 's/:.*//g')
+
+# Run the test suite
+
+cd $TEST_DIR
+CLUSTER_ADDR=$CLUSTER_ADDR WSU_PATH=$WMCO_ROOT/tools/ansible/tasks/wsu/main.yaml go test -v .
 
 exit 0
