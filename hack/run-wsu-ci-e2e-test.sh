e5e925c88a45ab2dbc0a29d4cfcab18a32e31df2
Sebastian Soto <ssoto@redhat.com>
2019-11-07 16:06:53 -0500

    Enable wsu e2e tests
    This commit enables wsu end to end tests. The tests are able to run now
    that we have updated the build containers, which was done in:
    https://github.com/openshift/release/pull/5756

--- hack/run-wsu-ci-e2e-test.sh
+++ hack/run-wsu-ci-e2e-test.sh
@@ -6,13 +6,9 @@ set -o pipefail
 WMCO_ROOT=$(dirname "${BASH_SOURCE}")/..
 TEST_DIR=$WMCO_ROOT/tools/ansible/tasks/wsu/test/e2e
 
-#TODO: Currently exiting early as the CI operator job which runs this does not have pip installed,
-#      which is a requirement to run this script
-exit 0
-
+sleep 1000
 # Required packages to run the test suite
-sudo yum install -y python libselinux-python python-pip
-pip install --user ansible pywinrm
+pip2 install --user ansible pywinrm
 
 # The WSU playbook requires the cluster address, we parse that here using oc
 CLUSTER_ADDR=$(oc cluster-info | head -n1 | sed 's/.*\/\/api.//g'| sed 's/:.*//g')
