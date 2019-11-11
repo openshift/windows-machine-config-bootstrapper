ceae9437c36c9254d8f3f6a0e57f084f5ade2eb7
Sebastian Soto <ssoto@redhat.com>
2019-11-11 15:12:42 -0500

    [wsu] Enable e2e tests
    This commit enables wsu e2e tests now that we have an image that is
    capable of running them. The image was introduced in:
    https://github.com/openshift/release/pull/5842
    
    This commit also changes the Dockerfile, hack script, and the tests
    to allow the tests to run properly within OpenShift CI.

--- hack/run-wsu-ci-e2e-test.sh
+++ hack/run-wsu-ci-e2e-test.sh
@@ -3,19 +3,25 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-WMCO_ROOT=$(dirname "${BASH_SOURCE}")/..
+WMCO_ROOT=$(pwd)
 TEST_DIR=$WMCO_ROOT/tools/ansible/tasks/wsu/test/e2e
 
-#TODO: Currently exiting early as the CI operator job which runs this does not have pip installed,
-#      which is a requirement to run this script
-exit 0
+# Make gopath if doesnt exist
+mkdir -p $GOPATH
+
+# If current user cannot be found add it to /etc/passwd. This is the case when this is run by an OpenShift cluster,
+# as OpenShift uses an arbitrarily assigned user ID to run the container.
+if ! whoami; then
+  echo "Creating user"
+  echo "tempuser:x:$(id -u):$(id -g):,,,:${HOME}:/bin/bash" >> /etc/passwd
+  echo "tempuser:x:$(id -G | cut -d' ' -f 2)" >> /etc/group
+fi
 
 # The WSU playbook requires the cluster address, we parse that here using oc
 CLUSTER_ADDR=$(oc cluster-info | head -n1 | sed 's/.*\/\/api.//g'| sed 's/:.*//g')
 
 # Run the test suite
-
 cd $TEST_DIR
-CLUSTER_ADDR=$CLUSTER_ADDR WSU_PATH=$WMCO_ROOT/tools/ansible/tasks/wsu/main.yaml go test -v .
+GO_BUILD_ARGS=CGO_ENABLED=0 GO111MODULE=on CLUSTER_ADDR=$CLUSTER_ADDR WSU_PATH=$WMCO_ROOT/tools/ansible/tasks/wsu/main.yaml go test -v .
 
 exit 0
